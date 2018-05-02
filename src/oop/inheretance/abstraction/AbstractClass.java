d3a945609e47685d1a42c633a8cad505bc14c5ab
nexon543 <glebaks94@gmail.com>
2018-05-02 19:05:25 +0300

    java core and algorithms

--- src/oop/inheretance/abstraction/AbstractClass.java
+++
@@ -1,6 +0,0 @@
-package oop.inheretance.abstraction;
-
-public abstract class AbstractClass {
-	//public abstract static void  f(); //error
-	
-}
--- src/oop/polymorphism/B.java
+++
@@ -1,7 +0,0 @@
-package oop.polymorphism;
-
-public class B extends A{
-	int i;
-	public static void staticFunc() {System.out.println("Class B static method");};
-	
-}
